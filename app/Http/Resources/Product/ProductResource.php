4b772cc16deece138388f81e69f9e43020126dee
jvnagarjuna <nagarjuna.jonnadula@gmail.com>
2019-10-18 20:35:29 +0530

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -21,9 +21,19 @@ class ProductResource extends JsonResource
 
             'price' => $this->price,
 
-            'stock' => $this->stock,
+            'discount' => $this->discount,
 
-            'discount' => $this->discount
+            'stock' => $this->stock ==0 ? 'Out of Stock' : $this->stock,
+
+            'totalPrice' => round((1-($this->discount/100)) * $this->price, 2),
+
+            'rating' => $this->reviews->count() > 0
+                ? round($this->reviews->sum('star') / $this->reviews->count(), 2)
+            :'No rating yet',
+
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ],
         ];
     }
 }
