3983d309183b553b0a1fc15f1929f4f6bfffeb57
jvnagarjuna <nagarjuna.jonnadula@gmail.com>
2019-10-19 08:28:21 +0530

    product save added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -14,6 +16,12 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
     public function index()
     {
         //
@@ -37,9 +45,29 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         //
+        $product = new Product;
+
+        $product->name = $request->name;
+
+        $product->detail = $request->description;
+
+        $product->stock = $request->stock;
+
+        $product->price = $request->price;
+
+        $product->discount = $request->discount;
+
+        $product->save();
+
+        return response([
+
+            'data' => new ProductResource($product),
+
+        ], RESPONSE::HTTP_CREATED);
+
     }
 
     /**
